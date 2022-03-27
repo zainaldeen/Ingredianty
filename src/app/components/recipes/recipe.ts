2d2fbe747ad9b3888714718e0873af13e0aa574a
Zain AlDeen Fayod <zainaldeenfayod@gmail.com>
2022-03-27 18:27:54 -0400

    prepare Recipe model

---
+++ src/app/components/recipes/recipe.ts
@@ -0,0 +1,22 @@
+export class Recipe {
+  id: number;
+  name: string;
+  description: string;
+  imagePath: string;
+  //ingredients: Ingredient[];
+
+  constructor(
+    id: number,
+    name: string,
+    description: string,
+    imagePath: string,
+    //ingredients: Ingredient[]
+  ) {
+    this.id = id;
+    this.name = name;
+    this.description = description;
+    this.imagePath = imagePath;
+    // this.ingredients = ingredients;
+  }
+}
+
