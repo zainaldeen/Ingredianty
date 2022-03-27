5fbd6fbca486502f7c02bd1aef3c15a9e0514bd3
Zain AlDeen Fayod <zainaldeenfayod@gmail.com>
2022-03-27 14:34:11 -0400

    play around

---
+++ src/app/servers/servers.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+
+
+@Component({
+  selector: 'app-servers',
+  templateUrl: './servers.component.html',
+  styleUrls: ['./servers.component.css']
+})
+export class ServersComponent implements OnInit {
+  isAddingAvailable: boolean = true;
+  message : string = "No Server has added yet";
+  buttonMessage: string = "Add Server";
+  typedServer: string = "";
+  flage = true;
+  constructor() {
+  }
+
+  ngOnInit(): void {
+  }
+
+  addNewServer() {
+    if (this.flage) {
+      this.message = "Server has been added";
+      this.buttonMessage = "Remove Server";
+    }else {
+      this.message = "No Server has added yet";
+      this.buttonMessage = "Add Server";
+    }
+    this.flage = !this.flage;
+  }
+  addTypedServer(event: Event) {
+    this.typedServer = (<HTMLInputElement>event.target).value;
+  }
+}
